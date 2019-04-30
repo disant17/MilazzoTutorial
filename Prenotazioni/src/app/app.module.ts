a1bcb588323cca0522c73a8d1e36f11d3f812e87
Lucas2000s <lucmil2000@gmail.com>
2019-04-30 09:02:11 +0000

    ciao

--- Prenotazioni/src/app/app.module.ts
+++ Prenotazioni/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'; //Aggiungiamo qui
 
 import { AppComponent } from './app.component';
+import { PrenotazioneComponent } from './prenotazione/prenotazione.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrenotazioneComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule ,NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
